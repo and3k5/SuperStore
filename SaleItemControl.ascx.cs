c8ea4e0437f630da3e4aa21b6d86a48166c256af
Anders K <ande7353@gmail.com>
2013-10-09 10:10:19 +0200

    Created SaleItemControl

---
+++ SaleItemControl.ascx.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+
+// usage for this
+// <%@ Register TagPrefix="ss" TagName="saleItem" Src="~/SaleItemControl.ascx" %>
+// <ss:saleItem runat="server" image="data"></ss:saleItem>
+
+public partial class SaleItemControl : System.Web.UI.UserControl
+{
+    public string image = "";
+    public string vendor = "Google";
+    public string product = "Glasses";
+    public string description = "Lorem Ipsum blablablablablablablabla lang text hej hej hej 1234";
+    public double price = 3333.33;
+    protected void Page_Load(object sender, EventArgs e)
+    {
+
+    }
+}
