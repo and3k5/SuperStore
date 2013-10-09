1e23effcc11aff1868243e238a321547a5e3f730
Anders K <ande7353@gmail.com>
2013-10-09 14:32:08 +0200

    Changed SQL structure
    Images is stored as path's not binary data - it's an overkill using
    binary data for this project.

---
+++ InsertImage.aspx
@@ -0,0 +1,24 @@
+﻿<%@ Page Language="C#" AutoEventWireup="true" CodeFile="InsertImage.aspx.cs" Inherits="InsertImage" %>
+
+<!DOCTYPE html>
+
+<html xmlns="http://www.w3.org/1999/xhtml">
+<head runat="server">
+    <title></title>
+</head>
+<body>
+    <form id="form1" runat="server">
+    <div>
+        <asp:ListBox ID="ListBox1" runat="server" OnSelectedIndexChanged="ListBox1_SelectedIndexChanged" AutoPostBack="true"></asp:ListBox>
+        <br />
+        <asp:Image ID="Image1" runat="server" />
+        <br />
+        <asp:Label ID="Label1" runat="server" Text="Label"></asp:Label>
+        <br />
+        <asp:FileUpload ID="FileUpload1" runat="server" />
+        <br />
+        <asp:Button ID="Button1" runat="server" Text="Button" />
+    </div>
+    </form>
+</body>
+</html>
---
+++ InsertImage.aspx.cs
@@ -0,0 +1,65 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.IO;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+using System.Data.SqlClient;
+using System.Data;
+using System.Configuration;
+using System.Web.Configuration;
+
+public partial class InsertImage : System.Web.UI.Page
+{
+    private SqlConnection con;
+    protected void Page_Load(object sender, EventArgs e)
+    {
+        if (!IsPostBack)
+        {
+            SqlConnection con = new SqlConnection(WebConfigurationManager.ConnectionStrings["DatabaseSuperStoreConnectionString1"].ConnectionString);
+            SqlCommand cmd = new SqlCommand("SELECT ProductID FROM SaleItems", con);
+            con.Open();
+            if (con.State == ConnectionState.Open)
+            {
+                using (SqlDataReader reader = cmd.ExecuteReader())
+                {
+                    ListBox1.Items.Clear();
+                    while (reader.Read())
+                    {
+                        ListItem item = new ListItem(reader.GetInt32(0).ToString(), reader.GetInt32(0).ToString());
+                        ListBox1.Items.Add(item);
+                    }
+                }
+            }
+            con.Close();
+        }
+    }
+    protected void ListBox1_SelectedIndexChanged(object sender, EventArgs e)
+    {
+        SqlConnection con = new SqlConnection(WebConfigurationManager.ConnectionStrings["DatabaseSuperStoreConnectionString1"].ConnectionString);
+
+        String query = String.Format("SELECT Image FROM SaleItems WHERE ProductID = '{0}'", ListBox1.SelectedValue);
+        Label1.Text = query;
+        SqlCommand cmd = new SqlCommand(query, con);
+        con.Open();
+        if (con.State == ConnectionState.Open)
+        {
+            using (SqlDataReader reader = cmd.ExecuteReader())
+            {
+                while (reader.Read())
+                {
+                    if (!reader.IsDBNull(0))
+                    {
+                        Image1.ImageUrl = String.Format("img/{0}", reader.GetString(0));
+                    }
+                    else
+                    {
+                        Image1.ImageUrl = String.Empty;
+                    }
+                }
+            }
+        }
+        con.Close();
+    }
+}
